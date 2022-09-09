ed335702b62d3a13232b5468a6a6ac906214c771
Ryan Corrigal <ryan.corrigal@gmail.com>
2022-09-09 11:07:09 -0600

    Updated linux scripts.

---
+++ linux-scripts/kali/configure-kismet.sh
@@ -0,0 +1,4 @@
+sudo usermod -aG kismet tenma
+mkdir -p ~/kismet/logs
+sudo chown -R tenma:kismet ~/kismet
+echo "Must log out for changes to take effect."
---
+++ linux-scripts/kali/install-general-apps.sh
@@ -0,0 +1,1 @@
+sudo apt install keepassxc chromium grub-customizer -y
